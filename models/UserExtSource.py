5d2f79e46f7ad12602a344f60c06ca7a2b16ff8c
Peter Bolha <xbolhap@gmail.com>
2022-02-21 08:20:29 +0100

    feat: changed attributes of user ext source model

--- models/UserExtSource.py
+++ models/UserExtSource.py
@@ -2,13 +2,12 @@ from HasIdAbstract import HasIdAbstract
 
 
 class UserExtSource(HasIdAbstract):
-    def __init__(self, id, name, bean_name, type, attributes):
+    def __init__(self, id, name, login, user_id):
         super().__init__(id)
         self.name = name
-        self.bean_name = bean_name
-        self.type = type
-        self.attributes = attributes
+        self.login = login
+        self.user_id = user_id
 
     def __str__(self):
-        return f"id: {self.id} name: {self.name} bean_name: {self.bean_name} "\
-               f"type: {self.type} attributes: {self.attributes}"
+        return f"id: {self.id} name: {self.name} login: {self.login} " \
+               f"user_id: {self.user_id}"
